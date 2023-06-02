dd2fb55485412c95d8bb4876e598629147cf60d8
iamthepoe <lucagamerbrime@gmail.com>
2023-06-02 18:41:34 -0300

    add unix commands object

--- client/TerminalCommands.js
+++ client/TerminalCommands.js
@@ -2,3 +2,7 @@ const windowsCommands = {
 	compileCommand: `gcc ${file}.c -o ${file}.exe && .\\${file}.exe < ${file}.txt`,
 	removeCommand: `del "${file}.*"`,
 };
+
+const unixCommands = {
+	compileCommand: `gcc ${file}.c -o ${file} -lm && timeout 2s ./${file} < ${file}.txt`,
+};
