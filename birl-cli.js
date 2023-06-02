69e7dc1fc32c5361f4978cbb6f3814cc4b447a98
iamthepoe <lucagamerbrime@gmail.com>
2023-06-02 18:43:23 -0300

    remove if

--- birl-cli.js
+++ birl-cli.js
@@ -7,19 +7,18 @@ const birl = new BirlClient();
 const birlFilePath = process.argv[2];
 const input = process.argv[3];
 
-if (process.platform == 'win32')
-	try {
-		const birlCode = fs.readFileSync(birlFilePath, 'utf-8');
-		birl.executeCode(birlCode, input)
-			.then((result) => {
-				if (result.code != 200) {
-					console.error('Erro:');
-					console.error(result.error);
-				}
-			})
-			.catch((error) => {
-				console.error('Erro ao executar o código BIRL:', error);
-			});
-	} catch (error) {
-		console.error('Erro ao ler o arquivo BIRL:', error);
-	}
+try {
+	const birlCode = fs.readFileSync(birlFilePath, 'utf-8');
+	birl.executeCode(birlCode, input)
+		.then((result) => {
+			if (result.code != 200) {
+				console.error('Erro:');
+				console.error(result.error);
+			}
+		})
+		.catch((error) => {
+			console.error('Erro ao executar o código BIRL:', error);
+		});
+} catch (error) {
+	console.error('Erro ao ler o arquivo BIRL:', error);
+}
